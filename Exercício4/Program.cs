368615a4fbdf43aa09137059cc98d22e9b9d73f0
Gustavo Alves <gustavo240697@gmail.com>
2022-06-24 23:44:13 -0300

    Primeiro commit

---
+++ Exercício4/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EXERCIOCIO_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            decimal sp = 67836.43m, rj = 36678.66m, mg = 29229.88m, es = 27165.48m, outros = 19849.53m;
+            decimal valorTotal = 153621.66m;
+
+            Console.WriteLine("Escolha uma sigla dos Estados");
+            string valorDigitado = Console.ReadLine();
+
+            decimal valor;
+            switch (valorDigitado.ToUpper())
+            {
+                case "SP":
+                    valor = sp;
+                    break;
+
+                case "RJ":
+                    valor = rj;
+                    break;
+
+                case "MG":
+                    valor = mg;
+                    break;
+
+                case "ES":
+                    valor = es;
+                    break;
+                default:
+                    valor = outros;
+                    break;
+            }
+
+            decimal calculo = (valor * 100 / valorTotal);
+            Console.WriteLine($"O lucro do estado foi {decimal.Round(calculo, 2)}%");
+
+            Console.Read();
+        }
+    }
+}
---
+++ Exercício5/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicio_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite uma palavra");
+            string palavraRecebida = Console.ReadLine();
+            char[] palavraArray = palavraRecebida.ToCharArray();
+            char[] palavraInvertida = new char[palavraRecebida.Length];
+            
+            for (int i = 0; i < palavraRecebida.Length; i++)
+            {
+                palavraInvertida[i] = palavraArray [(palavraRecebida.Length - i)-1];
+            }
+
+            Console.WriteLine(palavraInvertida);
+            string palavra = new string(palavraInvertida);
+
+            if (palavraRecebida.Replace(" ", "").ToUpper() == palavra.Replace(" ", "").ToUpper())
+            {
+                Console.WriteLine("Parabéns é um palíndromo");
+            }
+            else
+            {
+                Console.WriteLine("Tente novamente, não é um palíndromo");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
